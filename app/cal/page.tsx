d52cba156c055af4d4772224717c0abdebfc2de4
Harrychangtw <pomelo.cw@gmail.com>
2025-10-10 16:50:28 +0800

    feat: add calendar redirect functionality for meeting scheduling and CV access

---
+++ app/cal/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation'
+
+export default function CalendarRedirect() {
+  redirect('https://calendar.notion.so/meet/harry-chang/ybit2gkx')
+}
---
+++ app/cv/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation'
+
+export default function CalendarRedirect() {
+  redirect('https://drive.google.com/file/d/1xINWH85b8W8XIEB90Mw3NlAdKl0-9j6i/view?usp=drive_link')
+}
