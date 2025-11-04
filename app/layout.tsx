6155278a91cb68724f4b4a28f165362f44fd57c3
Harrychangtw <pomelo.cw@gmail.com>
2025-11-04 11:15:46 +0800

    fix: Add Open Graph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import type React from 'react'
+import type { Metadata } from 'next'
 import { Space_Grotesk, Press_Start_2P, IBM_Plex_Sans } from 'next/font/google'
 
 const spaceGrotesk = Space_Grotesk({
@@ -15,7 +16,35 @@ const ibmPlexSans = IBM_Plex_Sans({
   display: 'swap',
 })
 
-
+export const metadata: Metadata = {
+  metadataBase: new URL('https://harrychang.me'),
+  title: {
+    default: 'Harry Chang',
+    template: '%s | Harry Chang',
+  },
+  description: 'Portfolio of Harry Chang',
+  openGraph: {
+    title: 'Harry Chang',
+    description: 'Portfolio of Harry Chang',
+    url: 'https://harrychang.me',
+    siteName: 'Harry Chang',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Harry Chang',
+    description: 'Portfolio of Harry Chang',
+    images: ['/og-image.png'],
+  },
+}
 
 export default function RootLayout({
   children,
